6ac787441fb09579b1572325d3fd9ccba90ac3e6
Akashdeep <akash.kashhyap@gmail.com>
2023-10-02 21:21:53 +0530

    adding priority to featured image

--- components/FeaturedImage.js
+++ components/FeaturedImage.js
@@ -1,17 +1,28 @@
 import { storyblokEditable } from "@storyblok/react";
+import Image from "next/image";
 
 const FeaturedImage = ({ blok }) => {
-    // console.log("img ==>", blok);
+  // console.log("img ==>", blok);
   return (
     <figure className="w-full z-20 relative" {...storyblokEditable(blok)}>
       {blok.image.filename && (
-        <img
-          className="w-full object-cover object-center rounded-2xl"
-          alt={blok.image.alt}
-          src={`${blok.image.filename}/m/`}
-          width={644}
-          height={429}
-        />
+        // <img
+        //   className="w-full object-cover object-center rounded-2xl"
+        //   alt={blok.image.alt}
+        //   src={`${blok.image.filename}/m/`}
+        //   width={644}
+        //   height={429}
+        // />
+        <div className="relative">
+          <Image
+            src={`${blok.image.filename}/m/`}
+            alt={blok.image.alt}
+            width={644}
+            height={429}
+            className="w-full object-cover object-center rounded-2xl"
+            priority={true}
+          />
+        </div>
       )}
     </figure>
   );
