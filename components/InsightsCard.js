a6b2290b77ccefc3303cb15744aed7f9396ff7ff
Akashdeep <akash.kashhyap@gmail.com>
2023-10-02 23:33:38 +0530

    updating Link in insight component

--- components/InsightsCard.js
+++ components/InsightsCard.js
@@ -9,10 +9,11 @@ const InsightsCard = ({ blok }) => {
   const isBackgroundImageLayout = blok?.backgroundImageLayout;
 
   return (
-    <Link href={blok?.button?.cached_url} legacyBehavior>
+    <Link href={blok?.button?.cached_url}>
       {!isBackgroundImageLayout ? (
-        <a className=" 01 group rounded-xl overflow-hidden">
-          <div className="relative pt-[50%] sm:pt-[70%] rounded-xl overflow-hidden">
+        <div className="group rounded-xl overflow-hidden">
+          <div className="relative rounded-xl overflow-hidden">
+            <div className="aspect-w-16 aspect-h-9">
               <Image
                 src={`${blok?.image?.filename}/m/`}
                 alt="Image Description"
@@ -21,7 +22,8 @@ const InsightsCard = ({ blok }) => {
                 className="w-full h-full absolute top-0 left-0 object-cover object-center group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-xl"
                 priority={true}
               />
-           
+            </div>
+
             {isSponsored ? (
               <span className="absolute top-0 right-0 rounded-tr-xl rounded-bl-xl text-xs font-medium bg-poppy-900 text-white py-1.5 px-3">
                 Sponsored
@@ -34,26 +36,12 @@ const InsightsCard = ({ blok }) => {
 
             <p className="mt-5 inline-flex items-center gap-x-1.5 text-poppy-900 decoration-2 group-hover:underline font-medium">
               Read more
-              <svg
-                className="w-2.5 h-2.5"
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-              >
-                <path
-                  d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                />
-              </svg>
             </p>
           </div>
-        </a>
+        </div>
       ) : (
-        <a
-          className={`group relative flex flex-col w-full min-h-[15rem] bg-center bg-cover rounded-xl hover:shadow-lg`}
+        <div
+          className={`group relative flex flex-col w-full min-h-[15rem] h-full bg-center bg-cover rounded-xl hover:shadow-lg`}
           style={{ backgroundImage: `url(${blok?.image?.filename}/m/)` }}
         >
           <div className="flex-auto p-4 md:p-6 [&>h2]:text-xl [&>h2]:text-white/[.9] [&>h2]:group-hover:text-white/[.9]">
@@ -78,7 +66,7 @@ const InsightsCard = ({ blok }) => {
               </svg>
             </div>
           </div>
-        </a>
+        </div>
       )}
     </Link>
   );
